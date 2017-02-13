34d2825f0f1f0e3b763f76a595e0026ed78bba44
Keoma Brun <keoma.brun@inria.fr>
2017-02-13 14:11:06 +0100

    [data] add avg in pdr_dist script

--- datasets/scripts/pdr_dist.py
+++ datasets/scripts/pdr_dist.py
@@ -98,6 +98,10 @@ def main():
         list_pdr.append(pdr)
         list_dist.append(dist)
 
+    # compute average values
+
+    df_avg = pd.DataFrame({"y": list_pdr, "x": list_dist}).groupby("x", as_index=False).mean().round(1)
+
     # write result
 
     path = "{0}/{1}/{2}/pdr_dist/many_to_many/".format(OUT_PATH, args.testbed, args.date)
@@ -107,6 +111,7 @@ def main():
     json_data = {
         "x": list_dist,
         "y": list_pdr,
+        "avg": [{'x': x, 'y': y} for (x, y) in df_avg.to_dict("split")["data"]],
         "label": "PDR over distance"
     }
 
